3b815d67171863c507638539aea94e7faf692306
Shashank Srivastava <shashank.ssriv@gmail.com>
2019-12-19 10:21:09 +0530

    Use Docker's absolute path

--- .deploy_scripts/deploy_dockerhub.sh
+++ .deploy_scripts/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+/usr/local/bin/docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/$GITHUB_REPO:$TAG .
-docker push $DOCKER_USER/$GITHUB_REPO:$TAG
+/usr/local/bin/docker build -f Dockerfile -t $DOCKER_USER/$GITHUB_REPO:$TAG .
+/usr/local/bin/docker push $DOCKER_USER/$GITHUB_REPO:$TAG
