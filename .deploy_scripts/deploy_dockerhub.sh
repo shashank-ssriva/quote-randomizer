fb89f9cd76444572428437841627f93b7ed89a79
Shashank Srivastava <shashank.ssriv@gmail.com>
2019-12-19 14:56:20 +0530

    Rename BRANCH to TRAVIS_BRANCH

--- .deploy_scripts/deploy_dockerhub.sh
+++ .deploy_scripts/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
-    TAG="$BRANCH"
+    TAG="$TRAVIS_BRANCH"
 fi
 docker build -f Dockerfile -t $DOCKER_USER/$GITHUB_REPO:$TAG .
 docker push $DOCKER_USER/$GITHUB_REPO:$TAG
