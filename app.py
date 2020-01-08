9b0fa859c0f0526264c1784abf5587e5535e7d33
Shashank Srivastava <cheakymickey@gmail.com>
2020-01-08 14:31:46 +0530

    Change Flask app port to 5001

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def get_author_bio(author_name):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
